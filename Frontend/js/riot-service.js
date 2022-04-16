232347b69feb33e7ee6746f1dd8417f0fd6fa47d
Tarik <tarik.lilic@stu.ibu.edu.ba>
2022-04-16 16:30:02 +0200

    searching for a player

--- Frontend/js/riot-service.js
+++ Frontend/js/riot-service.js
@@ -3,7 +3,7 @@ var RiotService = {
         var requestData = {};
 
         $.ajax({
-            url: 'Backend/code/rest/summoners/' + $('#SearchPlayer').val() + '/' + $('#RegionButton').val(),
+            url: 'Backend/code/rest/summoners/' + document.getElementById("SearchPlayerInput").value + '/' + document.getElementById('RegionButton').innerText,
             //$('summonerName') je search bar, $('region') bi trebalo biti onaj dropdown
             type: 'GET',
             contentType: "application/json",
