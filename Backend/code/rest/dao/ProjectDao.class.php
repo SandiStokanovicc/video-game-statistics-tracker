d6ccd0c7610949220f54df5ec7b21f71491b51e6
Sandi Stokanovic <sandi.stokanovic@stu.ibu.edu.ba>
2022-04-17 15:09:06 +0200

    svega 2

--- Backend/code/rest/dao/ProjectDao.class.php
+++ Backend/code/rest/dao/ProjectDao.class.php
@@ -1,125 +1,82 @@
 <?php
-  class TrackerDao{
-    public function register(){
-    //include config file
-    require_once config.php;
+    class BaseDao{
+        private $conn;
+        private $table_name;
 
-    //defining variables
-    $username = $password = $confirm_password = $username_error = $password_error = $confirm_password_error = $email = $email_error = "";
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        //validating email
-        if(empty(trim($_POST["email"]))){
-            $email_error = "Please enter an email";
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $email_error = "Invalid email format";
-        } else{
-            $sql = "SELECT id FROM user WHERE email = :email";
-            if($stmt = $pdo->prepare($sql)){
-                //bind variables to the statement
-                $stmt -> bindParam(":email", $param_email, PDO::PARAM_STR);
-                //set parameters
-                $param_email = trim($_POST["email"]);
-                if(stmt->execute()){
-                    if(stmt ->rowCount() == 1){
-                        $email_error = "Email already taken";
-                    } else {
-                        $email = trim($_POST["email"]);
-                    }
-                } else{
-                    echo "Something went wrong.";
-                }
-                unset($stmt);
-            }
+        public function __construct($table_name){
+            $this->table_name = $table_name;
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $schema = "riot";
+            $this->conn = new PDO("mysql:host=$servername; dbname=$schema", $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        //read objects
+        public function get_all(){
+            $stmt = $this -> conn ->prepare("Select * from".$this->table_name);
+            $stmt ->execute();
+            return $stmt -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        //validating username;
-        if(empty(trim($_POST["username"]))){
-            $username_error = "Please enter your username";
-        } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
-            $username_error = "The username can only contain letters numbers and underscores.";
-        } else {
-            $sql = "SELECT id From user WHERE username = :username";
-            if($stmt = $pdo->prepare($sql)){
-                //variales to the statement
-                $stmt -> bindParam(":username", $param_username, PDO::PARAM_STR);
-                //set parameters
-                $param_username = trim($_POST["username"]);
-                if(stmt->execute()){
-                    if($stmt -> rowCount() == 1){
-                        $username_error = "Username already taken"; 
-                    } else {
-                        $username = trim($_POST["username"]);
-                    }
-                } else{
-                    echo "Something went wrong.";
-                }
-                unset($stmt);
-            }
+        //get objets by id
+        public function get_by_id($id){
+            $stmt = $this -> conn ->prepare("Select * from".$this->table_name."where id = _id");
+            $stmt->execture(['id'=>$id]);
+            $result = $stmt ->fetchAll(PDO::FETCH_ASSOC);
+            return reset($result);
         }
-        //validating password
-        if(empty(trim($_POST["password"]))){
-            $password_error = "Please enter a password.";
-        }elseif (strlen(trim($_POST["password"]))<6 || strlen(trim($_POST["password"]))>32){
-            $password_error = "The password must be between 6 and 32 characters long";
-        }else{
-            $password = trim($_POST["password"]);
-        }
-        //validating confirm password
-        if(empty(trim($_POST["confirm_password"]))){
-            $confirm_password_error = "Please confirm password.";     
-        } else{
-            $confirm_password = trim($_POST["confirm_password"]);
-            if(empty($password_error) && ($password != $confirm_password)){
-                $confirm_password_error = "The passwords do not match";
+        // delete a record  
+        public function delete($id){
+            $stmt = $this->conn->prepare("delete from ".$this->table_name." where id = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+          }
+
+        // add a record
+        public function add($entity){
+            $query = "insert into ".$this->table_name." (";
+            foreach ($entity as $column => $value) {
+                $query .= $column.",";
             }
-        }
-    
-        //checking for errors before inserting
-        if(empty($username_error) && empty($password_error) && empty($confirm_password_error) && empty($email_error)){
-            //preparing insert statement
-            $sql = "INSERT INTO user (username, password, email) VALUES (:username, :password, :email)";
-
-            if($stmt = $pdo ->prepare($sql)){
-                //bind variables to statement as parameters
-                $stmt -> bindParam(":username", $param_username, PDO::PARAM_STR);
-                $stmt -> bindParam(":password", $param_password, PDO::PARAM_STR);
-                $stmt -> bindParam(":email", $param_email, PDO::PARAM_STR);
-                
-                //set params
-                $param_username = $username;
-                $param_email = $email;
-                $param_password = password_hash($password, PASSWORD_DEFAULT); //creates password hash using bcrypt algorithm + randomly generated salt
-                //execute statement
-                if($stmt->execute()){
-                    header("location: ?.php"); //replace later
-                } else {
-                    echo "Something went wrong";
-                }
-                //closing statement
-                unset($stmt);
+            $query = substr($query, 0, -2);
+            $query .= ") VALUES (";
+            foreach ($entity as $column => $value){
+                $query .= ":".$column.", ";
             }
-        } 
-        //close connection
-        unset($pdo);
-        echo "hljeb";
-      }      
-    }
-    }
+            $query = substr($query, 0, -2);
+            $query .= ")";
 
-  
+            $stmt =$this->conn->prepare($query);
+            $enttiy['id'] = $id;
+            $stmt->execute($entity);
+        }
+        
+        //update a record
+        public function update($id, $entity, $id_column = "id"){
+            $query = "UPDATE ".this->table_name." SET ";
+            foreach($entity as $name => $value){
+                $query .= $name ."= :". $name.",";
+            }
+            $query = substr($query, 0, -2);
+            $query .= "Where ${id_column} =:id";
 
-/*     public function get_all(){
-      $stmt = $this->conn->prepare("SELECT * FROM 'summoner'");
-      $stmt->execute();
-      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt=$this->conn->prepare($query);
+            $entity['id'] =$id;
+            $stmt ->execute($entity);
+        }
+
+        protected function query($query, $params){
+            $stmt = $this->conn->prepare($query);
+            $stmt = execute($params);
+            return $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        protected function query_unique($query, $params){
+            $result = $this ->query($query, $params);
+            return reset($results);
+        }
+
+
     }
-
-    public function add(){
-      $stmt = $this->conn->prepare("INSERT INTO 'summoner' (idsummoner, summonerName) VALUES (:idsummoner, :summonerName)");
-      $stat->execute(['description' => $description, 'playername' => $playername]);
-    }
-  }
-
-*/
 ?>
