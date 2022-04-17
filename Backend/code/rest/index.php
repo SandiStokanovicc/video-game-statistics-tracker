ee36413076cd59a7eeaafddf8d779aace448b1df
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-04-17 16:45:39 +0200

    registration require fixed

--- Backend/code/rest/index.php
+++ Backend/code/rest/index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 require_once __DIR__.'\..\..\vendor\autoload.php';
 require_once __DIR__.'/services/RiotService.class.php';
 require_once __DIR__.'/dao/ProjectDao.class.php';
-require_once __DIR__.'/../Frontend/Registration.class.php';
+require_once __DIR__.'/../../../Frontend/registration.class.php';
 
 Flight::register('riotService', 'RiotService');
 Flight::register('trackerDao', 'TrackerDAO');
