574530302f60191473f99d0967be1844cde57ef1
Sandi Stokanovic <sandi.stokanovic@stu.ibu.edu.ba>
2022-05-17 16:34:05 +0200

    bhuj

--- src/js/UserService.js
+++ src/js/UserService.js
@@ -33,11 +33,87 @@
 // });
 
 
+// var UserService = {
+//     init: function () {
+//         var token = localStorage.getItem("token");
+//         if (token) {
+//             window.location.replace("homepage.html");
+//         }
+
+
+//         $('#modalFormLog').validate({
+//             submitHandler: function (form) {
+//                 var user = Object.fromEntries((new FormData(form)).entries());
+//                 UserService.login(user);
+//             }
+//         });
+
+//         $('#modalFormSign').validate({
+//             submitHandler: function (form) {
+//                 var user = {};
+//                 user.username = $('#usernameSignUp').val();
+//                 user.password = $('#passwordSignUp').val();
+//                 user.email = $('#emailSignUp').val();
+//                 UserService.register(user);
+//             }
+//         });
+//     },
+
+//     login: function (user) {
+        
+//         $.ajax({
+//             type: "POST",
+//             url: '/video-game-statistics-tracker/src/rest/authentication/login',
+//             data: JSON.stringify(user),
+//             contentType: "application/json",
+//             dataType: "json",
+
+//             success: function (data) {
+//                 console.log(data);
+//                 localStorage.setItem("token", data.token);
+//                 window.location.replace("index.html");
+
+//             },
+//             error: function (XMLHttpRequest, textStatus, errorThrown) {
+//                 toastr.error(XMLHttpRequest.responseJSON.message);
+//             }
+//         });
+//     },
+//     logout: function(){
+//         localStorage.clear();
+//         window.location.replace("index.html");
+//       },
+
+
+//     register: function (user) {
+
+
+//         $.ajax({
+//             type: "POST",
+//             url: '/rest/authentication/register',
+//             data: JSON.stringify(user),
+//             contentType: "application/json",
+//             dataType: "json",
+
+//             success: function (data) {
+//                // $('#modalFormSign').modal('toggle');
+//                // localStorage.setItem("token", data.token);
+//                // toastr.success('You have been succesfully registered.');
+//                 //localStorage.clear();
+//                 console.log("knai1");
+//             },
+//             error: function (XMLHttpRequest, textStatus, errorThrown) {
+//                 console.log("knai2");
+//                 //toastr.error(XMLHttpRequest.responseJSON.message);
+//             }
+//         });
+//     }
+// }
 var UserService = {
     init: function () {
         var token = localStorage.getItem("token");
         if (token) {
-            window.location.replace("homepage.html");
+            window.location.replace("index.html");
         }
 
 
@@ -58,12 +134,10 @@ var UserService = {
             }
         });
     },
-
     login: function (user) {
-        
         $.ajax({
             type: "POST",
-            url: '/video-game-statistics-tracker/src/rest/authentication/login',
+            url: ' rest/authentication/login',
             data: JSON.stringify(user),
             contentType: "application/json",
             dataType: "json",
@@ -79,45 +153,31 @@ var UserService = {
             }
         });
     },
-    logout: function(){
+
+    logout: function () {
         localStorage.clear();
         window.location.replace("index.html");
-      },
-
+    },
 
     register: function (user) {
-                 $.post("", user)
-             .done(function () {
-                 // you will get response from your php page (what you echo or print)
-                 // show successfully for submit message
-                 $("#result").html();
-                 console.log("hljeb1");
-             })
-             .fail(function (response) {
-                 // Log the error to the console
-                 // show error
-                 $("#result").html('There is some error while submit');
-                 console.error("The following error occurred: " + response.responseText);
-             });
+        $.ajax({
+            type: "POST",
+            url: ' rest/authentication/register',
+            data: JSON.stringify(user),
+            contentType: "application/json",
+            dataType: "json",
 
-        // $.ajax({
-        //     type: "POST",
-        //     url: '/video-game-statistics-tracker/src/rest/authentication/register',
-        //     data: JSON.stringify(user),
-        //     contentType: "application/json",
-        //     dataType: "json",
+            success: function (data) {
+                $('#SignUpModal').modal('toggle');
+                localStorage.setItem("token", data.token);
+                toastr.success('You have been succesfully registered.');
+                localStorage.clear();
 
-        //     success: function (data) {
-        //        // $('#modalFormSign').modal('toggle');
-        //        // localStorage.setItem("token", data.token);
-        //        // toastr.success('You have been succesfully registered.');
-        //         //localStorage.clear();
-        //         console.log("knai2");
-        //     },
-        //     error: function (XMLHttpRequest, textStatus, errorThrown) {
-        //         console.log("knai2");
-        //         //toastr.error(XMLHttpRequest.responseJSON.message);
-        //     }
-        // });
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                toastr.error(XMLHttpRequest.responseJSON.message);
+            }
+        });
     }
 }
+
--- src/rest/dao/BaseDao.class.php
+++ src/rest/dao/BaseDao.class.php
@@ -12,8 +12,8 @@ class BaseDao{
   public function __construct($table_name){
     $this->table_name = $table_name;
     $servername = "localhost";
-    $username = "riot";
-    $password = "riot";
+    $username = "root";
+    $password = "root";
     $schema = "riot";
     $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
     // set the PDO error mode to exception
