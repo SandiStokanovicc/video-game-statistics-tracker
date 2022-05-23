2a72b064db54dc0be0d8c2c5c75492bac578cac8
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-05-23 17:42:50 +0200

    added partial filter to JSON

--- rest/services/RiotService.class.php
+++ rest/services/RiotService.class.php
@@ -10,6 +10,105 @@
       "X-Riot-Token: RGAPI-259883a2-e25a-4d0d-be41-73524f582617"
     );
     
+    private function setCurlOptions($ch, $url){
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    }
+    
+    // NEW STUFF THAT PRINTS FILTERED INFORMATION
+    private function getSummonerInfo($summonerName, $region){
+      $summonerName = str_replace(" ", "%20", $summonerName); // space replaced with "%20" for GET method. Doesn't work otherwise
+      $summonerName = htmlspecialchars($summonerName); // replaces < with &lt, > with &gt, etc. for avoiding XSS attacks
+      //$region = "eun1";
+
+      $ch = curl_init(); // initialize cURL_PHP connection
+      $url = 'https://' . $region .'.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $summonerName;
+     
+
+      $this->setCurlOptions($ch, $url);
+
+      $response = curl_exec($ch); // get results
+
+      curl_close($ch); // close connection
+
+      $json = json_decode($response, true); // transform result from JSON (or whatever) into array
+      //return $json;
+
+      // or 
+      return array('name' => $json['name'], 'puuid' => $json['puuid']);
+    }
+
+    private function getSummonerMatchesPrivate($puuid, $continent){
+      $ch = curl_init();
+
+      //$puuid je path parameter (mora biti unutar url-a), dok su $start i $count query params (optional)
+      //$start = 0;
+      //$count = 10;
+      //$url = 'https://' . $requestData['continent'] . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $requestData['puuid'] . '/ids?start=0' . $start . '&count=10' . $count;
+
+      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $puuid . '/ids?start=0&count=2';
+
+      $this->setCurlOptions($ch, $url);
+
+      $response = curl_exec($ch);
+      $json = json_decode($response, true);
+      return $json;
+    }
+
+    private function getMatchInfo($matchId, $continent){
+      $ch = curl_init();
+      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/' . $matchId;
+      $this->setCurlOptions($ch, $url);
+
+      $response = curl_exec($ch);
+      $json = json_decode($response, true);
+      return $json = $this->filterInfo($json['info']);
+    }
+
+    private function filterInfo($info){
+      //return $info['participants'] = $this->filterParticipants($info['participants']);
+      $parts = $this->filterParticipants($info['participants']);
+
+      return array('info' => array('participants' => $parts));
+      //return $info = filterParticipants($info['participants']);
+    }
+
+    private function filterParticipants($participants){
+      $returnVal = array();
+      $i = 0;
+      while($i<10){
+        array_push($returnVal, $participants[$i]['summonerName']);
+        $i++;
+      }
+      return $returnVal;
+    }
+
+    // DINO KECO
+    public function getSummonerMatches($summonerName, $region){
+      if($region == "na1"){
+        $continent = "americas";
+      }
+      else{
+        $continent = "europe";
+      }
+      $summoner = $this->getSummonerInfo($summonerName, $region);
+      $j = 0;
+      $summoner['matches'] = $this->getSummonerMatchesPrivate($summoner['puuid'], $continent);
+      foreach($summoner['matches'] as $i => $match){
+
+        //$summoner['matches'][$i] = getMatchInfo($match['matchid']);
+        $summoner['matches'][$i] = $this->getMatchInfo($match, $continent, $j);
+        $j++;
+      }
+      return $summoner;
+      //exclude useless info
+    }
+
+
+
+    /* //OLD STUFF THAT PRINTS EVERYTHING COMBINED INTO 1 JSON
     private function getSummonerInfo1($summonerName, $region){
       $summonerName = str_replace(" ", "%20", $summonerName); // space replaced with "%20" for GET method. Doesn't work otherwise
       $summonerName = htmlspecialchars($summonerName); // replaces < with &lt, > with &gt, etc. for avoiding XSS attacks
@@ -76,14 +175,7 @@
       return $summoner;
       //exclude useless info
     }
-
-    private function setCurlOptions($ch, $url){
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    }
-  
+    */
   
 
     /*
