3192b2d4fd8d9dc645e04656da6294060b880956
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-05-23 10:20:10 +0200

    backend json connected

--- rest/routes/RiotRoutes.php
+++ rest/routes/RiotRoutes.php
@@ -1,15 +1,16 @@
 <?php
 
-// Flight::route('GET /summoners/@summonerName/@region', function($summonerName, $region){ 
-//   // trenutno se poziva sa 
-//   // http://localhost/video-game-statistics-tracker/Backend/code/rest/summoners/Condemn%20for%20Stun/eun1
-//   Flight::json(Flight::riotService()->getSummonerInfo($summonerName, $region));
-// });
-
-
 Flight::route('GET /summoners/@summonerName/@region', function($summonerName, $region){ 
-  Flight::json(Flight::riotService()->getSummonerInfo($summonerName, $region));
-});
+   //trenutno se poziva sa 
+   //http://localhost/video-game-statistics-tracker/Backend/code/rest/summoners/Condemn%20for%20Stun/eun1
+   //Flight::json(Flight::riotService()->getSummonerInfo($summonerName, $region));
+   Flight::json(Flight::riotService()->getSummonerMatches($summonerName, $region));
+ });
+
+
+//Flight::route('GET /summoners/@summonerName/@region', function($summonerName, $region){ 
+  //Flight::json(Flight::riotService()->getMatchBySummoner($summonerName, $region));
+//});
 
 /* //ne moze na ovaj nacin jer je post. Treba proslijediti samo request()->data->getData()
 Flight::route('POST /matches/@puuid/@continent', function($puuid, $continent){
--- rest/services/RiotService.class.php
+++ rest/services/RiotService.class.php
@@ -9,63 +9,84 @@
       "Origin: https://developer.riotgames.com",
       "X-Riot-Token: RGAPI-259883a2-e25a-4d0d-be41-73524f582617"
     );
-    /*
-    public function getMatchBySummoner($summonerName, $region){
+    
+    private function getSummonerInfo1($summonerName, $region){
       $summonerName = str_replace(" ", "%20", $summonerName); // space replaced with "%20" for GET method. Doesn't work otherwise
       $summonerName = htmlspecialchars($summonerName); // replaces < with &lt, > with &gt, etc. for avoiding XSS attacks
       //$region = "eun1";
 
       $ch = curl_init(); // initialize cURL_PHP connection
       $url = 'https://' . $region .'.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $summonerName;
-      
+     
+
       $this->setCurlOptions($ch, $url);
 
       $response = curl_exec($ch); // get results
 
-      $jsonSummonerInfo = json_decode($response, true); // transform result from JSON (or whatever) into array
-      
-      if($region == "na1"){
-        $continent = "americas";
-      }
-      else{
-        $continent = "europe";
-      }
-//-----------------------------------------------------------------------------------------------------------
+      curl_close($ch); // close connection
 
+      $json = json_decode($response, true); // transform result from JSON (or whatever) into array
+      return $json;
+
+      // or 
+      // return array('name' => $json['name'], 'something' => $json['something']);
+    }
+
+    private function getSummonerMatchesPrivate($puuid, $continent){
       $ch = curl_init();
 
       //$puuid je path parameter (mora biti unutar url-a), dok su $start i $count query params (optional)
       //$start = 0;
       //$count = 10;
       //$url = 'https://' . $requestData['continent'] . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $requestData['puuid'] . '/ids?start=0' . $start . '&count=10' . $count;
-      $matches=array();
-      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $jsonSummonerInfo['puuid'] . '/ids?start=0&count=2';
+
+      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $puuid . '/ids?start=0&count=2';
 
       $this->setCurlOptions($ch, $url);
 
       $response = curl_exec($ch);
-      $jsonMatchIDs = json_decode($response, true);
-      foreach($jsonMatchIDs as $matchId){
+      $json = json_decode($response, true);
+      return $json;
+    }
 
-      //-----------------------------------------------------------------------------------------------------------
+    private function getMatchInfo($matchId, $continent){
       $ch = curl_init();
       $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/' . $matchId;
       $this->setCurlOptions($ch, $url);
-  
+
       $response = curl_exec($ch);
-      array_merge($matches,json_decode($response, true));
-    }
-      curl_close($ch); // close connection
       $json = json_decode($response, true);
-      return $json;    
+      return $json;
+    }
 
+    // DINO KECO
+    public function getSummonerMatches($summonerName, $region){
+      if($region == "na1"){
+        $continent = "americas";
       }
-
-      public function returnMatches($response){
-        return $response;
-
+      else{
+        $continent = "europe";
       }
-    */
+      $summoner = $this->getSummonerInfo1($summonerName, $region);
+      $summoner['matches'] = $this->getSummonerMatchesPrivate($summoner['puuid'], $continent);
+      foreach($summoner['matches'] as $i => $match){
+        //$summoner['matches'][$i] = getMatchInfo($match['matchid']);
+        $summoner['matches'][$i] = $this->getMatchInfo($match, $continent);
+      }
+      return $summoner;
+      //exclude useless info
+    }
+
+    private function setCurlOptions($ch, $url){
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    }
+  
+  
+
+    /*
     public function getSummonerInfo($summonerName, $region){
       $summonerName = str_replace(" ", "%20", $summonerName); // space replaced with "%20" for GET method. Doesn't work otherwise
       $summonerName = htmlspecialchars($summonerName); // replaces < with &lt, > with &gt, etc. for avoiding XSS attacks
@@ -73,7 +94,7 @@
 
       $ch = curl_init(); // initialize cURL_PHP connection
       $url = 'https://' . $region .'.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $summonerName;
-      
+     
 
       $this->setCurlOptions($ch, $url);
 
@@ -113,13 +134,82 @@
     $response = curl_exec($ch);
     $json = json_decode($response, true);
     return $json;
-  }
+  }*/
   
-  private function setCurlOptions($ch, $url){
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    public function getMatchBySummoner($summonerName, $region){
+      $fullReturn = array();
+      $summonerName = str_replace(" ", "%20", $summonerName); // space replaced with "%20" for GET method. Doesn't work otherwise
+      $summonerName = htmlspecialchars($summonerName); // replaces < with &lt, > with &gt, etc. for avoiding XSS attacks
+      //$region = "eun1";
+
+      $ch = curl_init(); // initialize cURL_PHP connection
+      $url = 'https://' . $region .'.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $summonerName;
+     
+      $this->setCurlOptions($ch, $url);
+
+      $jsonSummonerInfo = curl_exec($ch); // get results
+
+      $afullReturn = json_decode($response, true); // transform result from JSON (or whatever) into array
+      
+      if($region == "na1"){
+        $continent = "americas";
+      }
+      else{
+        $continent = "europe";
+      }
+//-----------------------------------------------------------------------------------------------------------
+
+      $ch = curl_init();
+
+      //$puuid je path parameter (mora biti unutar url-a), dok su $start i $count query params (optional)
+      //$start = 0;
+      //$count = 10;
+      //$url = 'https://' . $requestData['continent'] . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $requestData['puuid'] . '/ids?start=0' . $start . '&count=10' . $count;
+      $matches=array();
+      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/by-puuid/' . $jsonSummonerInfo['puuid'] . '/ids?start=0&count=2';
+
+      $this->setCurlOptions($ch, $url);
+
+      $responseMatches = curl_exec($ch);
+
+      $SummonerAndMatchIDs = 
+      return $responseMatches;
+      //$jsonMatchIDs = json_decode($response, true);
+      foreach($jsonMatchIDs as $matchId){
+
+      //-----------------------------------------------------------------------------------------------------------
+      $ch = curl_init();
+      $url = 'https://' . $continent . '.api.riotgames.com/lol/match/v5/matches/' . $matchId;
+      $this->setCurlOptions($ch, $url);
+  
+      $responseSpecificMatch = curl_exec($ch);
+      array_merge($matches,json_decode($response, true));
+    }
+    
+      curl_close($ch); // close connection
+      return $matches;
+
+      //$json = json_decode($response, true);
+      //return $json;    
+      }
+
+      public function returnMatches($response){
+        return $response;
+
+      }
+  */
 }
 
