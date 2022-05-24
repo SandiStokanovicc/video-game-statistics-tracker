cdbcc16040d29014a8547a10fa54adeacadadf00
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-05-24 18:51:01 +0200

    RIOT API key updated

--- rest/services/RiotService.class.php
+++ rest/services/RiotService.class.php
@@ -7,7 +7,7 @@
       "Accept-Language: en-US,en;q=0.9",
       "Accept-Charset: application/x-www-form-urlencoded; charset=UTF-8",
       "Origin: https://developer.riotgames.com",
-      "X-Riot-Token: RGAPI-d14a97a1-86d9-483d-92e3-60216a91e8db"
+      "X-Riot-Token: RGAPI-46857b73-6c13-4195-a579-fa50e6831409"
     );
     
     private function setCurlOptions($ch, $url){
