14a95f1e0d4cb4934705ea1279d4f99658b6fadb
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-06-07 17:56:44 +0200

    finally fixed Mobile API route

--- rest/index.php
+++ rest/index.php
@@ -31,8 +31,10 @@ Flight::route('/*', function(){
     //return TRUE;
     //perform JWT decode
     $path = Flight::request()->url;
+    //var_dump($path); die;
+    // /summonersMobileAPI/Condemn%20for%20Stun/eun1
     if ($path == '/login' || $path == '/register' || $path == '/docs.json' 
-    || $path == '/summonersMobileAPI/@summonerName/@region') return TRUE; // exclude routes from middleware
+    || str_starts_with($path, '/summonersMobileAPI/')) return TRUE; // exclude routes from middleware
   
     $headers = getallheaders();
     if (@!$headers['Authorization']){
