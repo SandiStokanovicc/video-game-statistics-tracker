09c5c1f1c170c2f911d6b36d07fff40e4640a2f7
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-06-28 15:39:57 +0200

    fixed docs

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -2,7 +2,7 @@
 
 
 /**
- * @OA\Info(title="ToDo API Specs", version="0.2", @OA\Contact(email="dino.keco@ibu.edu.ba", name="Dino Keco"))
+ * @OA\Info(title="ToDo API Specs", version="0.2", @OA\Contact(email="dzenis.kajevic@stu.ibu.edu.ba", name="Dzenis Kajevic"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost/video-game-statistics-tracker/rest", description="Development Environment" ),
  *    @OA\Server(url="https://stattrack.me/rest", description="Production Environment" )
