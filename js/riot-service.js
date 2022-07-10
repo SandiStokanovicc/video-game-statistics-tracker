5a299eedef2549b78667815e82abf8ebb5847c96
Tarik <tarik.lilic@stu.ibu.edu.ba>
2022-07-10 12:04:28 +0200

    fixed background bug

--- js/riot-service.js
+++ js/riot-service.js
@@ -106,21 +106,21 @@ var RiotService = {
                     </div>
                 </div>
                 </div>`;
-                html += `<button onclick="FavouriteService.addFavourite();">Add Favourite</button>`;
+                html += `<button type="button" onclick="FavouriteService.addFavourite()" class="btn btn-danger mb-5;">Add Favourite</button>`;
                 if (results.liveMatch.IsInMatch == true) {
                     html += `<button id="liveMatchButton" type="button" onclick="RiotService.showLiveMatch()" class="btn btn-success mb-5;">
                     Check Live Game
                 </button>
-                <div class="container" style="display: none" id="liveMatchFull">`;
+                <div class="container mt-4 mb-5" style="display: none" id="liveMatchFull">`;
                     for (i = 0; i < 10; i++) {
                         html += `<div class="container" id="livematch"><div class="row"><div class="col text-center"><p class="text-break mt-3 livematchtext"><br>` + results.liveMatch.participants[i].summonerName + `</p></div>
-                        <div class="col "><img class="shadow championicons mt-2" src="Pictures/champion/` + results.liveMatch.participants[i].championId + `.png" alt="ChampName"</img></div>
+                        <div class="col "><img class="shadow championicons mt-2 mb-2" src="Pictures/champion/` + results.liveMatch.participants[i].championId + `.png" alt="ChampName"</img></div>
                         <div class="col d-flex justify-content-start"><img class="shadow summspell mt-4 me-2" src="Pictures/summonerSpells/` + results.liveMatch.participants[i].summonerSpell1Id + `.png" alt="ChampName"></img>
                         <img class="shadow summspell mt-4" src="Pictures/summonerSpells/` + results.liveMatch.participants[i].summonerSpell2Id + `.png" alt="ChampName"></img></div>
                         <div class="col"><p class="text-break livematchtext"><br>Banned Champion:</p></div>`;
                         if (results.liveMatch.bannedChampions[i] == undefined) continue;
                         html += `
-                    <div class="col"><img class="shadow championicons mt-2" src="Pictures/champion/` + results.liveMatch.bannedChampions[i] + `.png" alt="ChampName" width="30" height="30"></img></div></div></div>`;
+                    <div class="col"><img class="shadow championicons mt-2 mb-2" src="Pictures/champion/` + results.liveMatch.bannedChampions[i] + `.png" alt="ChampName" width="30" height="30"></img></div></div></div>`;
                     }
                     html += `</div>`;
                 }
