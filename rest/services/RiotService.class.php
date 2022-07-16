7a4a7de0e44423fa963672de288e3bdcb96875d4
Sandi Stokanovic <sandi.stokanovic@stu.ibu.edu.ba>
2022-07-16 12:46:38 +0200

    api key

--- rest/services/RiotService.class.php
+++ rest/services/RiotService.class.php
@@ -7,7 +7,7 @@
       "Accept-Language: en-US,en;q=0.9",
       "Accept-Charset: application/x-www-form-urlencoded; charset=UTF-8",
       "Origin: https://developer.riotgames.com",
-      "X-Riot-Token: RGAPI-a7e9b419-1c1f-441d-bfb5-808ff03d956a" //RGAPI-a7e9b419-1c1f-441d-bfb5-808ff03d956a
+      "X-Riot-Token: RGAPI-715f08ff-eee3-4cea-a317-ca3182a68df7" //RGAPI-a7e9b419-1c1f-441d-bfb5-808ff03d956a
     );
     
     private function setCurlOptions($ch, $url){
