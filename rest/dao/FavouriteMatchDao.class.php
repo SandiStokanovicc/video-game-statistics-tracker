f2a969664f0b4ff522cf5ad67be2bafd209a52eb
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-07-16 14:30:23 +0200

    changed fav match delete

--- rest/dao/FavouriteMatchDao.class.php
+++ rest/dao/FavouriteMatchDao.class.php
@@ -14,10 +14,6 @@ class FavouriteMatchDao extends BaseDao {
   public function getIdMatchIDContinent($userId, $APIMatchID, $continent){
     return $this->query_unique("SELECT * FROM favmatches WHERE userId = :userId AND APIMatchID = :APIMatchID AND continent = :continent", ['userId' => $userId, 'APIMatchID' => $APIMatchID, 'continent' => $continent]);
   }
-
-  public function deleteFavouriteMatch($userId, $APIMatchID, $continent){
-    return $this->query("DELETE FROM favmatches WHERE userId = :userId AND APIMatchID = :APIMatchID AND continent = :continent", ['userId' => $userId, 'APIMatchID' => $APIMatchID, 'continent' => $continent]);
-  }
   
   public function countFavMatchesByID($userId){
     return $this->query_unique("SELECT COUNT(*) AS count FROM favmatches WHERE userId = :userId", ['userId' => $userId]);
